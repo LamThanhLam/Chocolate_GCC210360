794e3a5baa644ca8634e26d9a966013464cf96a3
LamThanhLam <lamltgcc210360@fpt.edu.vn>
2023-08-07 20:19:28 +0700

    sdfgbsdfg

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require_once('header.php');
+?>
+        <section class="py-5 text-center container "
+        style="background-image: url('img/pexels-joão-jesus-925743.jpg'); height:600px">
+            <div class="row py-lg-5">
+                <div class="col-lg-6 col-md-8 mx-auto">
+                    <h1 class="fw-light">Million Dollar Shop</h1>
+                    <p class="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing 
+                        elit. Ut facilisis est sit amet tortor viverra, eget convallis leo ornare. 
+                        Nunc molestie placerat diam ut cursus. Praesent lacus lorem, semper viverra 
+                        lectus ut, euismod eleifend ante. Curabitur purus diam, tristique nec justo 
+                        a, vehicula aliquet est. Pellentesque vitae vierra nisi. Praesent ac est sit 
+                        amet nisi lobortis rutrum tincidunt sit amet elit. Ut placerat tincidunt erat 
+                        et luctus.
+                         .</p>
+                    <h2 class="pb-2 border-bottom">Hot Products</h2>
+                </div>
+            </div>
+            <div class="b-example-divider"></div>
+            <footer class="text-muted py-5">
+                <div class="container">
+                    <p class="float-end mb-1">
+                        <a href="index.php">Back to top</a>
+                    </p>
+                    <p>&copy; 2017-2022 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a
+                    href="#">Terms</a></p>
+                </div>
+            </footer>
+        </section>
+
+
+    </body>
+</html>
