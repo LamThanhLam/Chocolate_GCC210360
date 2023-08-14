ee5e76d38b673610424b55b07cca080943ce410e
LamThanhLam <lamltgcc210360@fpt.edu.vn>
2023-08-14 08:30:20 +0700

    AfterExport

---
+++ shop_gcc210360.sql
@@ -0,0 +1,129 @@
+-- MariaDB dump 10.17  Distrib 10.4.8-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: shop_gcc210360
+-- ------------------------------------------------------
+-- Server version	10.4.8-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `shop_gcc210360`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `shop_gcc210360` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `shop_gcc210360`;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `Cat_ID` varchar(10) NOT NULL,
+  `Cat_Name` varchar(30) NOT NULL,
+  `Cat_Des` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`Cat_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES ('C001','Bar','Chocolate bar type'),('C002','Candy','Chocolate candy type');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `Username` varchar(20) NOT NULL,
+  `Password` varchar(40) NOT NULL,
+  `CustName` varchar(30) NOT NULL,
+  `gender` int(11) NOT NULL,
+  `Address` varchar(1000) NOT NULL,
+  `telephone` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `CusDate` int(11) NOT NULL,
+  `CusMonth` int(11) NOT NULL,
+  `CusYear` int(11) NOT NULL,
+  `SSN` varchar(10) DEFAULT NULL,
+  `ActiveCode` varchar(100) NOT NULL,
+  `state` int(11) NOT NULL,
+  PRIMARY KEY (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES ('Lazlo','e10adc3949ba59abbe56e057f20f883e','Lam Thanh Lam',0,'Ca Mau','0913674594','lamthanhlam20062003@gmail.com',20,6,2003,'','',0),('Mao','e10adc3949ba59abbe56e057f20f883e','Lam Thanh Lam',0,'Can Tho','0913674594','lamltgcc210360@fpt.edu.vn',20,6,2003,'','',0);
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `Product_ID` varchar(10) NOT NULL,
+  `Product_Name` varchar(60) NOT NULL,
+  `Price` decimal(13,2) NOT NULL,
+  `oldPrice` decimal(12,2) NOT NULL,
+  `SmallDesc` varchar(1000) NOT NULL,
+  `DetailDesc` text NOT NULL,
+  `ProDate` datetime NOT NULL,
+  `Pro_qty` int(11) NOT NULL,
+  `Pro_image` varchar(200) NOT NULL,
+  `Cat_ID` varchar(10) NOT NULL,
+  PRIMARY KEY (`Product_ID`),
+  KEY `Cat_ID` (`Cat_ID`),
+  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`Cat_ID`) REFERENCES `category` (`Cat_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES ('P001','Iwanka',5.00,0.00,'Made in Russia','<p>\r\n	The name was inspired from an old funny Russian song, this product is very cheap to the American but very expensive to the Vietnamese</p>\r\n','2023-08-13 17:53:05',15,'istockphoto-186682188-612x612.jpg','C001'),('P002','Milky Winie',10.00,0.00,'For children','<p>\r\n	Made in US</p>\r\n','2023-08-13 18:09:04',5,'chocolate-bar-2-min_4000x.jpg','C001');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-14  8:28:58
